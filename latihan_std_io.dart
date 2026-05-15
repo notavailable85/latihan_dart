ab2c21e5c920519e161971680a53a9e5ea6fea12
Crash App <email.notavailable85@gmail.com>
2026-05-15 20:29:42 +0700

    latihan_std_out

---
+++ latihan_std_io.dart
@@ -0,0 +1,26 @@
+// import 'dart:io';
+
+// void main() {
+//   // Meminta input gaji dari pengguna
+//   stdout.write("Masukkan gaji Anda:");
+
+//   String? inputGaji = stdin.readLineSync();
+//   // Membaca input sebagai String
+
+//   // Mengubah string input menjadi angka (int)
+//   int gaji = int.parse(inputGaji ?? "0");
+
+//   // Menentukan pajak 10% dari gaji
+//   double pajak = gaji * 0.1;
+
+//   // Menampilkan hasil
+//   print("Gaji Anda: Rp $gaji");
+//   print("Pajak (10%): Rp $pajak");
+// }
+import 'dart:io';
+
+void main() {
+  stdout.writeln("Program berjalan");
+
+  stderr.writeln("Ada kesalahan");
+}
